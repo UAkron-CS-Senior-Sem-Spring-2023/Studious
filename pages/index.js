e55300d107d700a3c1fa36e2bd795217f2070274
BSand <bradsandorf@gmail.com>
2023-03-04 15:03:13 -0500

    fix error on page reload

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,50 @@ import Image from 'next/image'
 import Link from 'next/link'
 import LoginSignupForm from './login'
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from 'react';
+
 
 // check if the user is logged in- if not redirect them to the login page
 function CheckForLogin() {
-  if (typeof window !== 'undefined') {
-    if (localStorage.getItem('email') === null) {
-      window.location.href = '/login';
-    } else {
-      // return (
-      //   <h1>{localStorage.getItem('email')}</h1>
-      // )
-      alert(localStorage.getItem('email'));
+  const [email, setEmail] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('email') === null) {
+        window.location.href = '/login';
+      } else {
+        setEmail(localStorage.getItem('email'));
+      }
     }
+  }, []);
+
+  if (email) {
+    return <h1>{email}</h1>;
+  } else {
+    return null;
+  }
+}
+
+// returns the email
+function DisplayEmail() {
+  const [email, setEmail] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setEmail(localStorage.getItem('email'));
+    }
+  }, []);
+
+  if (email) {
+    return <h1>{email}</h1>;
+  } else {
+    return null;
   }
 }
 
 // displays the schedule
 function DisplaySchedule() {
-  
+
 }
 
 export default function Home() {
