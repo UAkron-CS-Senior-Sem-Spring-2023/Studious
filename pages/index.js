b04bfb37752ae792603020cbc0d471cfc39c7dc4
BSand <bradsandorf@gmail.com>
2023-03-04 14:36:22 -0500

    using local storage to set the user's email

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,24 @@ import Link from 'next/link'
 import LoginSignupForm from './login'
 import styles from '@/styles/Home.module.css'
 
+// check if the user is logged in- if not redirect them to the login page
+function CheckForLogin() {
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('email') === null) {
+      window.location.href = '/login';
+    } else {
+      // return (
+      //   <h1>{localStorage.getItem('email')}</h1>
+      // )
+      alert(localStorage.getItem('email'));
+    }
+  }
+}
 
+// displays the schedule
+function DisplaySchedule() {
+  
+}
 
 export default function Home() {
   return (
@@ -17,12 +34,15 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           src="/studioustransparent.png"
-          width={300}
-          height={150}
+          width={150}
+          height={75}
         />
-        <LoginSignupForm/>
+        
+        {/* On load, check if the user is logged in. If they are not, redirect them to the login page, which will redirect them back here after they login */}
+        <CheckForLogin />
+        <DisplaySchedule />
+
       </main>
     </>
   )
 }
-
