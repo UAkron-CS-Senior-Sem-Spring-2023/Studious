cc4de2f8ec3dedc839736a727531c0a725f81914
Mitchell Ruple <mjr160@uakron.edu>
2023-04-17 10:25:15 -0400

    Changed all styling to Chakra Components

--- pages/signup.js
+++ pages/signup.js
@@ -1,31 +1,39 @@
-import React, {useState} from 'react';
-import Router from 'next/router';
-import cookie from 'js-cookie';
-import cx from 'classnames'
-import styles from 'styles/Login.module.css'
-import Link from 'next/link';
-import Image from 'next/image';
+import React, { useState } from "react";
+import Router from "next/router";
+
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Image,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 
 const Signup = () => {
-    const addUser = async (event) => {
+  const addUser = async (event) => {
     event.preventDefault();
 
+    const formData = new FormData(event.target);
+
     const data = {
-        email: event.target.email.value,
-        password: event.target.password.value,
-        first_name: event.target.first_name.value
+      email: formData.get("email"),
+      password: formData.get("password"),
+      first_name: formData.get("first_name"),
     };
 
     const JSONdata = JSON.stringify(data);
-    const endpoint = '/api/users';
+    const endpoint = "/api/users";
 
     const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/JSON',
-        },
-        body: JSONdata,
-    }
+      method: "POST",
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+      body: JSONdata,
+    };
 
     const response = await fetch(endpoint, options);
     const result = await response.json();
@@ -33,42 +41,62 @@ const Signup = () => {
     // check if the post was successful
     if (result.success) {
       // set the localsession variable
-      localStorage.setItem('email', event.target.email.value);
-      localStorage.setItem('first_name', event.target.first_name.value);
+      localStorage.setItem("email", formData.get("email"));
+      localStorage.setItem("first_name", formData.get("first_name"));
 
       // redirect the user back to the home page
-      window.location.href = '/';
+      Router.push("/");
     }
-  }
+  };
 
-  //form onSubmit should call addUser
   return (
-    <main className={cx(styles["form-signin"],"text-center","mt-5")}>
-        <Image
-          src="/studioustransparent.png"
-          width={300}
-          height={150}
-        />
-        <br />
-        <br />
-        <br />
-      <form onSubmit={addUser}>
-        <div className="form-floating">
-          <input type="email" className="form-control" id="email" name="email" placeholder="name@example.com" />
-          <label htmlFor="email">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input type="text" className="form-control" id="first_name" name="first_name" placeholder="Name" />
-          <label htmlFor="first_name">First name</label>
-        </div>
-        <div className="form-floating">
-          <input type="password" className="form-control" id="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Create Account</button>
-      </form>
-      <p>Return to Login? <Link href="/"> Click here</Link></p>
-    </main>
+    <Box display="flex" alignItems="center" justifyContent="center" h="100vh">
+      <Box p={8} maxW="md" borderWidth={1} borderRadius={8} boxShadow="lg">
+        <Image src="/studioustransparent.png" width={300} height={150} />
+        <form onSubmit={addUser}>
+          <FormControl mt={4}>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+            />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel htmlFor="first_name">First name</FormLabel>
+            <Input
+              type="text"
+              id="first_name"
+              name="first_name"
+              placeholder="Name"
+            />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </FormControl>
+          <Button
+            bg="black" // add black background here
+            color="white" // keep white text color here
+            size="lg"
+            mt={4}
+            width="100%"
+            type="submit"
+          >
+            Create Account
+          </Button>
+          <Text mt={4}>
+            Return to Login? <Link href="/"> Click here</Link>
+          </Text>
+        </form>
+      </Box>
+    </Box>
   );
 };
 
